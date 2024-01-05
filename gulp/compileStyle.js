4860e40346794674fc3b2c7e9e855edbe94d8587
Nail Markov <markov_nail@mail.ru>
2024-01-05 13:53:59 +0300

    Добавляет конфигурацию stylelint

--- gulp/compileStyle.js
+++ gulp/compileStyle.js
@@ -1,11 +1,14 @@
+import autoprefixer from 'autoprefixer';
 import csso from 'gulp-csso';
 import dartSass from 'sass';
 import gulp from 'gulp';
 import gulpSass from 'gulp-sass';
+import postcss from 'gulp-postcss';
 import notify from 'gulp-notify';
 import rename from 'gulp-rename';
 
 const sass = gulpSass(dartSass);
+const postcssPlagins = [autoprefixer()]
 
 // Import from files
 
@@ -15,14 +18,14 @@ import { pathSrc, pathBuild } from './config.js';
 
 // Task
 
-const stylesBuild = () => {
-  return gulp
+const stylesBuild = () =>
+  gulp
     .src(`${pathSrc.scss}/*.scss`, { sourcemaps: true })
     .pipe(sass().on('error', notify.onError(errorMessages('scss'))))
+    .pipe(postcss(postcssPlagins))
     .pipe(csso())
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest(`${pathBuild.css}`, { sourcemaps: '.' }))
     .pipe(server.stream());
-}
 
 export { stylesBuild }
